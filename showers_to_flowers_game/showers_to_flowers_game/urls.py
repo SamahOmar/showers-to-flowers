0ee6c64f11f2a535927cff5b6f387351fd48f14f
SamahOmar <samahomardahle@gmail.com>
2026-05-21 13:07:31 +0300

    add SVG favicon under the app’s static files, link it in the shared base template, and route /favicon.ico to that asset so both explicit and browser-default   requests stop returning 404.

--- showers_to_flowers_game/showers_to_flowers_game/urls.py
+++ showers_to_flowers_game/showers_to_flowers_game/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.templatetags.static import static
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=static("game/img/favicon.svg"), permanent=True),
+        name="favicon",
+    ),
     path('', include('game.urls')),
-    
 ]
