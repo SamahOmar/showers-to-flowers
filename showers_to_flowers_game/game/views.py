14872b385dfbe68977c263c177c7801d41e04353
SamahOmar <samahomardahle@gmail.com>
2026-07-19 19:07:56 +0300

    game/views.py: change the Context that returns from the View to the game tab. add a list of Daily Affirmations (pink inspirational and self-empowerment phrases) that will be randomly selected from the server, and passed into the HTML.

--- showers_to_flowers_game/game/views.py
+++ showers_to_flowers_game/game/views.py
@@ -1,5 +1,34 @@
+import random
 from django.shortcuts import render
+from .models import GameSession
 
-# Create your views here.
-def index(request):
-    return render(request, 'game/index.html')
+
+def game_view(request):
+    # Creating or merging the existing session
+    session_key = request.session.session_key
+    if not session_key:
+        request.session.create()
+        session_key = request.session.session_key
+
+    game_session, created = GameSession.objects.get_or_create(session_key=session_key)
+
+    # list of Daily Affirmations
+    daily_affirmations = [
+        "Pour yourself an iced latte and let your inner flower bloom. 🌸✨",
+        "High-shine, low-stress. You are doing amazing sweetie! 💅🏼💖",
+        "Protect your peace, write your code, and chase the sunset. 🌅💻",
+        "Slow down. Just like flowers, you need rain and time to grow. ✨🌷",
+        "Manifesting error-free code and unlimited glitter-pink vibes today. 🎀"
+    ]
+
+    # randomly selected
+    selected_affirmation = random.choice(daily_affirmations)
+
+    context = {
+        'game_session': game_session,
+        'current_growth': game_session.current_growth,
+        'glam_points': game_session.glam_points,
+        'daily_affirmation': selected_affirmation,
+    }
+
+    return render(request, 'game/game.html', context)
